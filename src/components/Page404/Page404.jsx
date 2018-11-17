56ef66ceb5797d4584d72d3b9874f5d322bd6bbc
Denis Khomichenko <simkhotech@gmail.com>
2018-11-17 04:03:22 +0300

    Added Page404

---
+++ src/components/Page404/Page404.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import PropTypes from 'prop-types';
+import Alert from '../Alert';
+
+const Page404 = ({ type, head, message }) => (
+  <React.Fragment>
+    <Alert type={type} head={head} message={message} />
+    <Button
+      bsStyle="primary"
+      href="/"
+    >
+      Back Home
+    </Button>
+  </React.Fragment>
+);
+
+Page404.propTypes = {
+  type: 'info',
+  head: '404',
+  message: PropTypes.string,
+};
+
+Page404.defaultProps = {
+  type: 'info',
+  head: '404',
+  message: 'There does not seem to be a page here',
+};
+
+export default Page404;
