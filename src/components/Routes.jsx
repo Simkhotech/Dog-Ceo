2388f4a948ed25a33c5f6bf0a1677bd83d29e23a
Denis Khomichenko <simkhotech@gmail.com>
2018-11-21 00:01:02 +0300

    updated deprecated loadable-components to @loadable/component. Also updated webpack to 4.26.x

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,4 +1,4 @@
-import loadable from 'loadable-components';
+import loadable from '@loadable/component';
 import LoadingSpinner from './LoadingSpinner/LoadingSpinner';
 
 export const BreedList = loadable(() => import('../containers/BreedListPageContainer'), {
